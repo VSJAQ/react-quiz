6a17f71e7b5ddca87fb6ae1f30995fc4944a103c
Erik Devyashin <vsjaq25@gmail.com>
2021-01-13 09:21:54 +0500

    added menu list

---
+++ src/components/navigation/drawer/Drawer.js
@@ -0,0 +1,35 @@
+import React, { Component } from 'react';
+import styles from './Drawer.module.css';
+
+const links = ['quiz', 'menu', 'contacts']
+
+export class Drawer extends Component {
+
+  renderLinks() {
+    return ( links.map((link, index) => {
+      return (
+        <li key={index}>
+          <a>
+            {link}
+          </a>
+        </li>
+      )}))
+  }
+
+  render() {
+    const classNameArr = [styles.drawer]
+
+    if (!this.props.isOpen) {
+      classNameArr.push(styles.close)
+    }
+
+    return (
+      <nav className={classNameArr.join(' ')}>
+        <ul>
+          { this.renderLinks() }
+        </ul>
+      </nav>
+    )
+  }
+
+}
--- src/hoc/Layout.js
+++ src/hoc/Layout.js
@@ -1,6 +1,7 @@
 import React, {Component} from "react";
 import styles from './Layout.module.css'
 import { MenuToggle } from "../components/navigation/menuToggele/MenuToggle";
+import { Drawer } from "../components/navigation/drawer/Drawer";
 
 class Layout extends Component {
 
@@ -17,6 +18,7 @@ class Layout extends Component {
   render() {
     return (
       <div className={styles.Layout}>
+        <Drawer isOpen={this.state.isOpen}/>
         <MenuToggle
           onToggle={this.toggleMenuHandler}
           isOpen={this.state.isOpen}
